8b892f991d52eb2a6c09854a43eb5fc15bd257ec
Tilman Kamp <tilmankamp@posteo.de>
2016-06-02 10:59:50 +0200

    Building java bindings
    Signed-off-by: Tilman Kamp <tilmankamp@posteo.de>

---
+++ build-java-bindings.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+root=`pwd`
+set -e
+
+echo "##### Fetching sphinxbase..."
+rm -rf sphinxbase
+git clone --depth 1 https://github.com/mozilla/sphinxbase
+rm -rf sphinxbase/.git
+
+echo "##### Building sphinxbase..."
+cd sphinxbase
+./autogen.sh --without-python --with-pic --enable-static=yes --prefix=$root/target
+make
+make install
+cd ..
+
+echo "##### Building pocketsphinx..."
+./autogen.sh --without-python --with-pic --enable-static=yes --with-sphinxbase=$root/sphinxbase --prefix=$root/target
+make
+make install
+
+echo "##### Generating swig libraries..."
+cd swig
+make
+
+echo "##### Generating jni module..."
+cd java
+make
+cd ..
+
+cd ..
